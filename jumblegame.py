8064836a3c12fb379f02dd9876a1dc44b6920d14
Mlee3333 <166416296+Mlee3333@users.noreply.github.com>
2025-04-20 10:22:43 +0100

    Add files via upload

--- jumblegame.py
+++ jumblegame.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import time 
 from tkinter import font as f
+import time
 import random
 
 animals = {"easy": ["cat", "dog", "bat", "cow", "fox", "rat", "ant", "bee", "pig", "owl"],
@@ -23,31 +23,83 @@ def jumble(word):
     return jumbled
 
 def selectscreen():
-    choosef.tkraise()
-
-def start():
-    d = varD.get()
-    c = varC.get()
+    global score,count,word
+    choosef.tkraise()   
     score = 0
     count = 0
-    while count < 5:
-        n = random.randint(0,9)
-        word = allwords[c][d][n]
-        t = varT.get()
-        t = int(t)
-        timel.config(text="Timer:\n{}s".format(t))
-        jumbled = jumble(word)
-        juml.config(text=jumbled)
-        guessf.tkraise()
-        while t > 0:
-            main.update()
-            time.sleep(1)
-            t -= 1
-            timel.config(text="Timer:\n{}s".format(t))
-        if t <= 0:
-            count += 1
+    word = ""
+    scorel.config(text="Score: {}/{}".format(score,count))
 
-        scorel.config(text="Score: {}/{}".format(score,count))
+def endscreen():
+    endingf.tkraise()
+    title2 = Label(endingf,bg="blue",fg="white",text="Game over:\nYou scored: {}/{} ".format(score,count),font=font1)
+    title2.grid(row=1,column=1,padx=30,pady=30)
+    ng2 = Button(endingf,text="New game",font=font1,command=selectscreen)
+    ng2.grid(row=2,column=1,padx=10,pady=10)
+    
+def start():
+    global d,c,t
+    d = varD.get()
+    c = varC.get()
+    t = varT.get()
+    setup()
+
+def setup():
+    global score,count, d,c,t,word, tleft
+    if count == 5:
+        endscreen()
+        return
+
+    word = random.choice(allwords[c][d]).lower()
+    tleft = int(t)
+    timel.config(text="Timer:\n{}s".format(t))
+    jumbled = jumble(word)
+    juml.config(text=jumbled)
+    guessf.tkraise()
+    timing()
+   
+def timing():
+    global count, tleft,current
+    if tleft > 0:
+        tleft -= 1
+        timel.config(text="Timer:\n{}s".format(tleft))
+        current = main.after(1000,timing)
+    else:
+        count += 1
+        setup()
+
+def guess():
+    global word, score, count, current
+    guessW = entry.get().lower()
+    if guessW == word:
+        score += 1
+        entry.config(fg="lime")
+    else:
+        entry.config(fg="red")
+    count += 1
+    main.after_cancel(current)
+    scorel.config(text="Score: {}/{}".format(score,count))
+    def contin():
+        entry.delete(0, END)
+        entry.config(fg="black")
+        setup()
+    main.after(500,contin)
+
+def reset():
+    global score,count,word
+    entry.delete(0,END)
+    main.after_cancel(current)
+    score = 0
+    count = 0
+    word = ""
+    setup()
+
+def skip():
+    global score,count, d,c,word
+    entry.delete(0,END)
+    word = random.choice(allwords[c][d]).lower()
+    jumbled = jumble(word)
+    juml.config(text=jumbled)
 
 main = Tk()
 font1 = f.Font(family="calibri",weight="bold",size=18)
@@ -122,23 +174,29 @@ startb.grid(row=6,column=2)
 title2 = Label(guessf,bg="green",fg="white",text="Unjumble the words: ",font=font1)
 title2.grid(row=1,column=1,padx=10,pady=10)
 
-juml = Label(guessf,bg="white",fg="green",text="",font=font1,width=15)
+juml = Label(guessf,bg="white",fg="blue",text="",font=font1,width=15)
 juml.grid(row=2,column=1,padx=10,pady=10)
 
 entry = Entry(guessf,font=font1)
 entry.grid(row=3,column=1,padx=30,pady=10)
 
-submit = Button(guessf,text="Submit",font=font1,bg="white",fg="green")
+submit = Button(guessf,text="Submit",font=font1,bg="white",fg="blue",command=guess)
 submit.grid(row=4,column=1,padx=30,pady=10)
 
-reset = Button(guessf,text="Reset",font=font1,bg="black",fg="red")
-reset.grid(row=2,column=2,padx=30,pady=10)
+resetbtn = Button(guessf,text="Reset",font=font1,bg="black",fg="red",command=reset)
+resetbtn.grid(row=2,column=2,padx=30,pady=10)
+
+skipbtn = Button(guessf,text="Skip",font=font1,bg="black",fg="lime",command=skip)
+skipbtn.grid(row=3,column=2,padx=30,pady=10)
 
 scorel = Label(guessf,bg="green",fg="white",text="Score: 0/0",font=font1)
 scorel.grid(row=1,column=2,padx=10,pady=10)
 
-timel = Label(guessf,bg="white",fg="green",text="",font=font1,width=10)
+timel = Label(guessf,bg="white",fg="blue",text="",font=font1,width=10)
 timel.grid(row=4,column=2,padx=10,pady=10)
+
+endingf = Frame(main,bg="blue")
+endingf.grid(row=1,column=1,sticky="nsew")
 
 startf.tkraise()
 main.mainloop()
